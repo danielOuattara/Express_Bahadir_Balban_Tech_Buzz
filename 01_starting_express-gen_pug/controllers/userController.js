b1199d8b57816de0b53ba98074f0639427bbb61e
daniel ouattara <daniel.ouattara@gmx.com>
2021-12-13 23:10:25 +0100

    starting

---
+++ 01_starting_express-gen_pug/controllers/userController.js
@@ -0,0 +1,4 @@
+
+exports.userController = function(req, res, next) {
+  res.send('respond with a resource');
+};
---
+++ 01_starting_express-gen_pug/routes/usersRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+/* GET users listing. */
+router.get('/',userController.userController );
+
+module.exports = router;
